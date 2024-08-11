fc0720ddc63ca4024392a26768614c38ea3cac43
KJ Kastning <kjk@skyhopper.net>
2024-08-11 17:05:16 -0400

    Mobile nav bar color and text change

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">' + 'BAAP' + '</span>' + //$('#logo').html()
 				'</div>'
 			)
 				.appendTo($body);
