75d477d3560eb0c65465c0430bf3e027a61c2f59
Avi Sharma <64670742+Av1Sharma@users.noreply.github.com>
2026-08-06 02:12:53 -0400

    food

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,12 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true,
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        about: resolve(import.meta.dirname, 'about.html'),
+      },
+    },
   },
   server: {
     port: 3000,
